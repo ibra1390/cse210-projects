db524b79f34ac8f1826acfd98640371989f2f66e
Ibra <ing.gonzalezvij@hotmail.com>
2023-01-06 22:00:48 -0600

    first part added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,30 @@ class Program
     static void Main(string[] args)
     {
         // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        if (percent >= 90)
+        {
+            Console.WriteLine("A");   
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
     }
 }
